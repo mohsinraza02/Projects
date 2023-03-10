bc635c205281ac0cc338dd4cf761ebe5cddc5466
mohsinraza02 <47283171+mohsinraza02@users.noreply.github.com>
2023-03-10 14:15:39 -0700

    Update BusinessName.py

--- BusinessName.py
+++ BusinessName.py
@@ -1,4 +1,5 @@
+print("Welcome to the business name generator")
 yourName = input("What is the your name?")
 businessType = input("What type of business do you run?")
 
-print(yourName + " " + businessType)
+print("The name of your business is: " + yourName + " " + businessType)
